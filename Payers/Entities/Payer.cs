4377086eda60b81fb0aefc4cd0cc4d54d89c4761
Diego Schinemann <diego.alexandre@hotmail.com>
2021-12-09 10:59:38 -0300

    criado construtor

--- Payers/Entities/Payer.cs
+++ Payers/Entities/Payer.cs
@@ -11,6 +11,12 @@ namespace Payers.Entities
         public string Name { get; private set; }
         public double Income { get; private set; }
 
+        public Payer(string name, double income)
+        {
+            Name = name;    
+            Income = income;
+        }
+
         public abstract double tax();
     }
 }
