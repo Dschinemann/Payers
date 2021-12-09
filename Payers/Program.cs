eb2f77857bca54a4832bd3a6e32af6061d083850
Diego Schinemann <diego.alexandre@hotmail.com>
2021-12-09 11:00:56 -0300

    Main

--- Payers/Program.cs
+++ Payers/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Payers.Entities;
+using System.Globalization;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter the number of tax payers: ");
+        int n = int.Parse(Console.ReadLine());
+        List<Payer> payers = new List<Payer>();
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Tax payer #{i+1} data:");
+            Console.Write("Individual or company (i/c)?");
+            bool isCompany = Console.ReadLine().Equals("c") ? true : false;
+            Console.Write("Name:");
+            string name = Console.ReadLine();
+            Console.Write("Anual income: ");
+            double income = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (isCompany)
+            {
+                Console.Write("Number of employees: ");
+                int numberOfEmployess = int.Parse(Console.ReadLine());
+                payers.Add(new Company(numberOfEmployess, name,income)) ;
+            }
+            else
+            {
+                Console.Write("Health expenditures: ");
+                double healthExpenditures = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                payers.Add(new Individual(healthExpenditures,name,income));
+            }
+
+        }
+        Console.WriteLine("TAXES PAID:");
+        double totalTaxes = 0;
+        foreach (Payer payer in payers)
+        {
+            Console.WriteLine($"{payer.Name}: $ {payer.tax():F2}");
+            totalTaxes += payer.tax();
+        }
+        Console.WriteLine($"TOTAL TAXES: $ {totalTaxes:F2}");
+
+    }
+}
